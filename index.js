39fb1e89ae43cfb5c772a60dec55465bc41e300e
MeganJade5 <98874054+MeganJade5@users.noreply.github.com>
2022-09-22 21:30:32 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let homeTotal = document.getElementById("homeScore()")
+let guestTotal = document.getElementById("guestScore()")
+
+function num1(){
+    homeTotal.textContent +=1
+    guestTotal.textContent +=1
+}
+
+function num2(){
+    homeTotal.textContent +=2
+    guestTotal.textContent +=2
+}
+
+function num3(){
+    homeTotal.textContent +=3
+    guestTotal.textContent +=3
+}
